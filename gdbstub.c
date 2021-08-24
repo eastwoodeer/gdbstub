a5ff370e9853a6bc976c3a04c05432acbce57f46
Chenbin <eastwoodeer@outlook.com>
2021-08-24 23:32:19 +0800

    add socket io

--- gdbstub.c
+++ gdbstub.c
@@ -6,12 +6,83 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+typedef unsigned char u8;
+typedef int i32;
+
 #define error(...)                                                             \
 	{                                                                      \
 		fprintf(stderr, __VA_ARGS__);                                  \
 		fflush(stderr);                                                \
 		exit(1);                                                       \
 	}
+
+struct gio {
+	u8 (*get_char)(void);
+	void (*put_char)(u8);
+	void (*init)(i32);
+};
+
+struct socket_io {
+	int fd;
+} socket_io;
+
+static int socket_io_init(int fd)
+{
+	socket_io.fd = fd;
+	return 0;
+}
+
+static u8 socket_get_char(void)
+{
+	u8 buf;
+	int fd = socket_io.fd;
+	int n = recv(fd, &buf, 1, 0);
+	if (!n) {
+		error("no more contents.\n");
+	}
+
+	if (n < 0) {
+		error("read failed.");
+	}
+
+	return buf;
+}
+
+static void socket_put_char(u8 ch)
+{
+	int fd = socket_io.fd;
+	if (send(fd, &ch, 1, 0) < 0) {
+		error("send failed.\n");
+	}
+}
+
+struct gio debug_io = {
+	.get_char = socket_get_char,
+	.put_char = socket_put_char,
+};
+
+static int _get_packet(u8 *buf, size_t buf_len, size_t *len)
+{
+	u8 ch = '0';
+
+	while (ch != '$') {
+		ch = debug_io.get_char();
+	}
+
+	*len = 0;
+	while (*len < buf_len - 1) {
+		ch = debug_io.get_char();
+
+		if (ch == '#') {
+			break;
+		}
+
+		buf[*len] = ch;
+		*len += 1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -50,23 +121,26 @@ int main(int argc, char *argv[])
 			error("Could not accept new connection.\n");
 		}
 		printf("accepted\n");
+		socket_io_init(client_fd);
 
 		while (1) {
-			char buf[1024] = { 0 };
-			int n = recv(client_fd, buf, 1024, 0);
-			if (!n) {
-				break;
-			}
+			u8 buf[1024] = { 0 };
+			/* int n = recv(client_fd, buf, 1024, 0); */
+			/* if (!n) { */
+			/* 	break; */
+			/* } */
 
-			if (n < 0) {
-				error("read failed.");
-			}
+			/* if (n < 0) { */
+			/* 	error("read failed."); */
+			/* } */
+			size_t len = 0;
+			_get_packet(buf, 1024, &len);
 
 			printf("recv: %s\n", buf);
 
-			sprintf(buf, "%c%c", '+', '\0');
+			sprintf(buf, "+$#00");
 
-			if (send(client_fd, buf, 2, 0) < 0) {
+			if (send(client_fd, buf, 5, 0) < 0) {
 				error("send failed.\n");
 			}
 		}
